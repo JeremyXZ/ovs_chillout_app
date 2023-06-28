0805a56b5478f58fe84b5d43c5be3457dd52ae12
JeremyXZ <jeremyuk168@gmail.com>
2023-06-28 17:00:15 +0100

    fixed bugs in unsplash api call

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,18 @@
-//getting images from Splash site
+//getting images from unsplash site
+const unsplashApiKey = process.env.REACT_APP_UNSPLASH_API_KEY;
 
 export async function getImage(setImageUrl) {
-  const numImagesAvailable = 2100; // total photos in the collection
-  // const numItemsToGenerate = 20; //how many photos you want to display
-  const imageWidth = 900; //image width in pixels
-  const imageHeight = 900; //image height in pixels
-  const collectionID = 928423; //Beach & Coastal, the collection ID from the original url
-  // collection ID: mountain: 401930 beach: 928423, landscape: 11649432, sea: 3403106 (find out by going to
-  // https://unsplash.com/collections/  click card and read number after collection/)
-  const randomNum = Math.floor(numImagesAvailable * Math.random());
-  const endPoint = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNum}`;
-  // "https://source.unsplash.com/collection/3672442/900x900/?sig=167"
-  //`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNum}`
+  const endPoint = `https://api.unsplash.com/photos/random?client_id=${unsplashApiKey}`;
 
   try {
     const res = await fetch(endPoint);
-    // const data = await res.json()
-    const data = res.url;
-    // const randomUrl = data[randomNum].urls.full
-    setImageUrl(data);
+    const data = await res.json();
+    const url = data.urls.regular;
+
+    setImageUrl(url);
 
     console.log("success to fetch");
+    console.log("unsplash api key", unsplashApiKey);
   } catch (err) {
     console.log("Error:", err);
   }
